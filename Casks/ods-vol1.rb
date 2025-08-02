9f2d02777598a49dcc015794f28a983dd8e7a617
Martim Lobao <dev@lobao.io>
2025-08-02 07:21:03 +0100

    release: Ohio Distinctive Software v1.0.0 - now on brew

--- Casks/ods-vol1.rb
+++ Casks/ods-vol1.rb
@@ -1,6 +1,6 @@
 cask "ods-vol1" do
     version "1.0.0"
-    sha256 "XXX"
+    sha256 "8797e22371ca5755ff2fb71e80d384ba1c01227c9b771213d6b5404079a83cf8"
 
     url "https://github.com/martimlobao/homebrew-dos-games/releases/download/ods-vol1-v#{version}/ods-vol1.app.zip"
     name "Ohio Distinctive Software Vol. I"
